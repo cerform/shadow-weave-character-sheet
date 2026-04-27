fd7364eb9d988b59ada44e82811f6e9cf6d73a54
cerform <im.se@outlook.com>
2026-04-27 20:25:14 +0300

    chore: cleanup binaries

--- redeploy.ps1
+++
@@ -1,4 +0,0 @@
-$env:SUPABASE_ACCESS_TOKEN = "sbp_ce92133a12e81c42999a965a03083a574c6f3565"
-Write-Host "Deploying ai-campaign-init (with worldSeed support)..."
-& ".\supabase_bin\supabase.exe" functions deploy ai-campaign-init --project-ref lalidbpqeobhzgtymcwu
-Write-Host "Done!"
